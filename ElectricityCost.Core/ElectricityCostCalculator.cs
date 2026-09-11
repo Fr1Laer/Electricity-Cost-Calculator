69abb1e335a7dd7e618bc9b42a420e70410b09de
Fr1Laer <az4561223@gmail.com>
2026-09-11 13:22:41 +0300

    Add project files.

---
+++ ElectricityCost.Core/ElectricityCostCalculator.cs
@@ -0,0 +1,13 @@
+﻿namespace ElectricityCost.Core
+{
+    public static class ElectricityCostCalculator
+    {
+        public static double Calculate(int powerConsumption, double usageHours, double energyPrice)
+        {
+            double powerInKilowatts = powerConsumption / 1000.0;
+            double result = powerInKilowatts * usageHours * energyPrice * 30;
+            
+            return result;
+        }
+    }
+}
---
+++ ElectricityCost.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using ElectricityCost.Core;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ElectricityCost.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(PowerTextBox.Text, out int powerConsumption))
+            {
+                MessageBox.Show("Palun sisestage kehtiv võimsustarbimine (W)");
+                return;
+            }
+
+            if (powerConsumption <= 0)
+            {
+                MessageBox.Show("Energiatarbimine peab olema suurem kui 0");
+                return;
+            }
+
+            if (!double.TryParse(UsageHoursTextBox.Text, out double usageHours))
+            {
+                MessageBox.Show("Palun sisestage kehtivad kasutustunnid");
+                return;
+            }
+
+            if (usageHours < 0 || usageHours > 24)
+            {
+                MessageBox.Show("Kasutustundide arv peab olema vahemikus 0–24");
+                return;
+            }
+
+            if (!double.TryParse(EnergyPriceTextBox.Text, out double energyPrice))
+            {
+                MessageBox.Show("Palun sisestage kehtiv energiahind (€/kWh). Näiteks: 0,0504 = 5,04 senti");
+                return;
+            }
+
+            if (energyPrice < 0)
+            {
+                MessageBox.Show("Energia hind ei saa olla negatiivne");
+                return;
+            }
+
+            double result = ElectricityCostCalculator.Calculate(powerConsumption, usageHours, energyPrice);
+
+            CostTextBox.Text = result.ToString("F2");
+        }
+    }
+}
